bf2b010b1efb663227af7a27502b49b06b8d211b
KaGen1999 <1032336124@qq.com>
2020-04-10 01:33:19 +0800

    No.17

---
+++ 17电话号码的字母组合.py
@@ -0,0 +1,32 @@
+class Solution:
+    def letterCombinations(self, digits: str):
+        d = {
+            '2': ['a', 'b', 'c'],
+            '3': ['d', 'e', 'f'],
+            '4': ['g', 'h', 'i'],
+            '5': ['j', 'k', 'i'],
+            '6': ['m', 'n', 'o'],
+            '7': ['p', 'q', 'r', 's'],
+            '8': ['t', 'u', 'v'],
+            '9': ['w', 'x', 'y', 'z']
+        }
+        result = []
+        s = ''
+        if digits == '':
+            return ''
+        def getString(s, digits):
+            if digits == '':
+                result.append(s)
+                return 0
+            else:
+                for i in d[digits[0]]:
+                    getString(s + i, digits[1:])
+
+        getString(s, digits)
+        return result
+
+
+if __name__ == '__main__':
+    s = Solution()
+    r = s.letterCombinations('23')
+    print(r)
